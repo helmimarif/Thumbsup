8386ded73eed8016588c5e658c7a2b6de9322271
helmimarif <helmimarif11@gmail.com>
2022-11-09 08:00:07 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Thumbsup
+
+### What is this?
+
+* This is a resource which adds the ability to thumbsup.
+
+### Features
+- To thumbsup press **`Del`**.
+
+## Download & Installation
+
+### Using Git
+```
+cd resources
+git clone https://github.com/helmimarif/Thumbsup
+```
+
+### Manually
+- Download from this.
+
+## Installation
+- Add this to your `server.cfg`:
+
+```
+start Thumbsup
+```
---
+++ client.lua
@@ -0,0 +1,21 @@
+Citizen.CreateThread(function()
+    local dict = "anim@mp_player_intincarthumbs_upbodhi@ps@"
+    
+	RequestAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        Citizen.Wait(100)
+    end
+    local thumbsup = false
+	while true do
+		Citizen.Wait(0)
+		if IsControlJustPressed(1, 178) then --Key Del
+            if not thumbsup then
+                TaskPlayAnim(GetPlayerPed(-1), dict, "thumbsup_enter", 8.0, 8.0, -1, 50, 0, false, false, false)
+                thumbsup = true
+            else
+                thumbsup = false
+                ClearPedTasks(GetPlayerPed(-1))
+            end
+        end
+    end
+end)
